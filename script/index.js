22ab0be79cd07d4f96799402f365b56ba6673ce4
sadia11-a <sadiasorna96@gmail.com>
2023-08-20 21:49:49 +0600

    button done

--- script/index.js
+++ script/index.js
@@ -9,14 +9,45 @@ function handleClickBtn(target) {
     selectedItemContainer.appendChild(ol);
      
     const price = target.parentNode.childNodes[1].childNodes[3].childNodes[5].innerText.split(' ')[0]
-    // console.log(target.parentNode.childNodes[1].childNodes[3].childNodes[5].innerText.split(' ')[0])
+   
       total = parseFloat(total) + parseFloat(price);
       const firstTotal = document.getElementById('total').innerText=total;
+      const disableBtn = document.getElementById('myButton');
+      if(firstTotal >= 200){
+        disableBtn.removeAttribute('disabled')
+      }
+      else{
+        disableBtn.setAttribute('disabled',true)
+      }
+      return firstTotal;
+
       
-    
-    
-    
+      
+      const discount = (firstTotal * 0.2).toFixed(2);
+      
+      const firstDiscount = document.getElementById('discount').innerText=discount;
+      const finalTotal1 = firstTotal - firstDiscount;
+      
+      const finalTotal = document.getElementById('final-total').innerText=finalTotal1;
+return finalTotal;
+    }
 
 
 
-}
+document.getElementById('text-coupon').addEventListener('keyup',function(e){
+  const text = e.target.value;
+  const applyBtn = document.getElementById('myButton').addEventListener('click',function(){
+    
+  })
+  if(text === 'SELL200'){
+    const discount = (firstTotal * 0.2).toFixed(2);
+    return discount;
+  }
+  
+  else{
+    return firstTotal;
+  }
+
+  const discount = (firstTotal * 0.2).toFixed(2);
+  const firstDiscount = document.getElementById('discount').innerText=discount;
+})
